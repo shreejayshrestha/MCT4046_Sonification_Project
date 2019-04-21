8d17d74917ff92ccd3d19fde34f485c80f5460d5
Shreejay Shrestha <shreejayshrestha@gmail.com>
2019-04-21 11:19:22 +0200

    edited javascript

---
+++ Data files/Javascript/sketch2.js
@@ -0,0 +1,104 @@
+
+let data1;
+var button;
+
+let osc1, osc2, osc3;
+let freq1, freq2,freq3;
+
+let arrayBus = [];
+let arraySE = [];
+let arrayWM = [];
+
+let figures1;
+
+function preload(){
+  loadJSON("Buses.json", dataReady1);
+}
+
+function dataReady1(data1){
+  osc1 = new p5.Oscillator();
+  osc2 = new p5.Oscillator();
+  osc3 = new p5.Oscillator();
+  
+  osc1.setType('sine');
+  osc1.freq(0);
+  osc1.amp(0.4);
+  osc1.start();
+
+  osc2.setType('triangle');
+  osc2.freq(0);
+  osc2.amp(0.5);
+  osc2.start();
+
+  osc3.setType('sawtooth');
+  osc3.freq(0);
+  osc3.amp(0.2);
+  osc3.start();
+
+  figures1 = data1.figures;
+  for (var i = 0; i < figures1.length; i++) {
+    append(arrayBus, [figures1[i].AADF,figures1[i].NE,figures1[i].SE,figures1[i].WM]);
+  };
+
+  // for three cities 
+  for (var i = 0; i < arrayBus.length;  i++) {
+    
+    setTimeout(function(y) {
+    
+    freq1 = map(arrayBus[y][1],140235, 183753, 200, 600);
+    freq2 = map(arrayBus[y][2],227700, 377048, 700, 1000);
+    freq3 = map(arrayBus[y][3],328924, 490877, 1100, 2000);
+
+    //console.log(freq);
+    
+    osc1.freq(freq1);
+    osc2.freq(freq2);
+    osc3.freq(freq3);
+    
+    }, i * 500, i);
+     // we're passing i  
+    };
+  }
+  
+function  setup() {
+
+  button = createButton('stop sonification');
+  button.position(150, 19);
+  button.mousePressed(stopsound);
+
+  button = createButton('start sonification');
+  button.position(19, 19);
+  button.mousePressed(startsound);
+  
+  //createCanvas(400,400);
+  noCanvas();
+
+  // create a sound recorder
+  recorder = new p5.SoundRecorder();
+
+  // connect the mic to the recorder
+  recorder.setInput();
+
+  // this sound file will be used to
+  // playback & save the recording
+  soundFile = new p5.SoundFile();
+  
+  function mousePressed(){
+    getAudioContext().resume()
+  } 
+
+  function startsound(){
+    osc1.start();
+    osc2.start();
+    osc3.start();
+    recorder.record(soundFile);
+  }
+
+  function stopsound() {
+    osc1.stop();
+    osc2.stop();
+    osc3.stop();
+    recorder.stop();
+    save(soundFile, 'mySound.wav');
+  }
+}
